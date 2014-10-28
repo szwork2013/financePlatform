be1631bc9a330048eaca88f789612ff58e86798a
luokui <kui.luo@sunlights.cc>
2014-10-28 17:44:23 +0800

    set dependencies.

--- project/Build.scala
+++ project/Build.scala
@@ -3,13 +3,6 @@ import sbt._
 import Keys._
 
 object FinancePlatformBuild extends Build {
-  lazy val root = Project(id = "financePlatform", base = file("."))
-    .dependsOn(common)
-    .dependsOn(trade)
-    .dependsOn(core)
-    .aggregate(common, trade, core, customer, account)
-    .settings(scalaVersion := "2.10.0")
-    .enablePlugins(PlayJava)
 
   lazy val common = Project(id = "common",base = file("common"))
     .settings(scalaVersion := "2.10.0")
@@ -17,10 +10,12 @@ object FinancePlatformBuild extends Build {
 
   lazy val trade = Project(id="trade", base= file("trade"))
     .settings(scalaVersion := "2.10.0")
+    .dependsOn(account)
     .dependsOn(common).enablePlugins(PlayJava)
 
   lazy val core = Project(id="core", base= file("core"))
     .settings(scalaVersion := "2.10.0")
+    .dependsOn(customer)
     .dependsOn(common).enablePlugins(PlayJava)
 
   lazy val customer = Project(id="customer", base= file("customer"))
@@ -30,4 +25,14 @@ object FinancePlatformBuild extends Build {
   lazy val account = Project(id="account", base= file("account"))
     .settings(scalaVersion := "2.10.0")
     .dependsOn(common).enablePlugins(PlayJava)
+
+  lazy val root = Project(id = "financePlatform", base = file("."))
+    .dependsOn(common)
+    .dependsOn(trade)
+    .dependsOn(core)
+    .dependsOn(customer)
+    .dependsOn(account)
+    .aggregate(common, trade, core, customer, account)
+    .settings(scalaVersion := "2.10.0")
+    .enablePlugins(PlayJava)
 }
