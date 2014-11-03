c42de8d62ea210269ee46b63818e5e78e4e919ef
luokui <kui.luo@sunlights.cc>
2014-11-03 12:05:10 +0800

    refactor code

--- common/app/com/sunlights/common/Severity.java
+++ common/app/com/sunlights/common/Severity.java
@@ -4,5 +4,19 @@ package com.sunlights.common;
  * Created by Administrator on 2014/11/3.
  */
 public enum Severity {
-    INFO, WARN, ERROR, FATAL
+    INFO(0), WARN(1), ERROR(2), FATAL(3)
+    ;
+
+    private int level;
+
+    Severity(int level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return ""+level;
+    }
+
+
 }
