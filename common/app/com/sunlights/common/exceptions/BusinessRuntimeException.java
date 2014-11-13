fa4e8e7fdfa09326a423a47bd1cd6182bd19a43f
Yuan Zhencai <zhencai.yuan@sunlights.cc>
2014-11-13 20:55:18 +0800

    business

--- common/app/com/sunlights/common/exceptions/BusinessRuntimeException.java
+++ common/app/com/sunlights/common/exceptions/BusinessRuntimeException.java
@@ -8,69 +8,75 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 
 public class BusinessRuntimeException extends RuntimeException {
-  private String errorCode;
-  private Severity severity;
-  private String detailMsg;
+    private Message message;
+	private String errorCode;
+    private Severity severity;
+    private String detailMsg;
 
-  public BusinessRuntimeException() {
-    super();
-  }
+	public BusinessRuntimeException() {
+		super();
+	}
 
-  public BusinessRuntimeException(String message) {
-    super(message);
-  }
+	public BusinessRuntimeException(String message) {
+		super(message);
+	}
 
-  public BusinessRuntimeException(String message, String errorCode) {
-    super(message);
-    this.errorCode = errorCode;
-  }
+	public BusinessRuntimeException(String message, String errorCode) {
+		super(message);
+		this.errorCode = errorCode;
+	}
 
-  public BusinessRuntimeException(String message, Throwable cause) {
-    super(message, cause);
-  }
+	public BusinessRuntimeException(String message, Throwable cause) {
+		super(message, cause);
+	}
 
-  public BusinessRuntimeException(String message, Throwable cause, String errorCode) {
-    super(message, cause);
-    this.errorCode = errorCode;
-  }
+	public BusinessRuntimeException(String message, Throwable cause, String errorCode) {
+		super(message, cause);
+		this.errorCode = errorCode;
+	}
 
-  public String getDetailMessage() {
-    Throwable cause = this.getCause();
-    if (cause == null) {
-      cause = this;
+	public String getDetailMessage() {
+		Throwable cause = this.getCause();
+		if (cause == null) {
+			cause = this;
+		}
+		StringWriter stringWriter = new StringWriter();
+		cause.printStackTrace(new PrintWriter(stringWriter));
+		return stringWriter.toString();
+	}
+
+	public String getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+
+    public BusinessRuntimeException(Message message){
+        super(message.getSummary());
+        this.message = message;
+        this.severity = message.currentSeverity();
+        this.errorCode = message.getCode();
+        this.detailMsg = message.getDetail();
     }
-    StringWriter stringWriter = new StringWriter();
-    cause.printStackTrace(new PrintWriter(stringWriter));
-    return stringWriter.toString();
-  }
 
-  public String getErrorCode() {
-    return errorCode;
-  }
+    public BusinessRuntimeException(Severity severity, String code, String summary, String detail){
+        super(summary);
+        this.errorCode = code;
+        this.severity = severity;
+        this.detailMsg = detail;
+    }
 
-  public void setErrorCode(String errorCode) {
-    this.errorCode = errorCode;
-  }
+    public Severity getSeverity(){
+        return severity;
+    }
+    public String getDetailMsg(){
+        return detailMsg;
+    }
 
-  public BusinessRuntimeException(Message message) {
-    super(message.getSummary());
-    this.severity = message.currentSeverity();
-    this.errorCode = message.getCode();
-    this.detailMsg = message.getDetail();
-  }
+    public Message getBusinessMessage() {
+        return message;
+    }
 
-  public BusinessRuntimeException(Severity severity, String code, String summary, String detail) {
-    super(summary);
-    this.errorCode = code;
-    this.severity = severity;
-    this.detailMsg = detail;
-  }
-
-  public Severity getSeverity() {
-    return severity;
-  }
-
-  public String getDetailMsg() {
-    return detailMsg;
-  }
 }
