f9a7bef6d473cf87d97e0cd7e35ffe7c13348efc
tangweiqun <weiqun.tang@sunlights.cc>
2014-11-13 16:23:05 +0800

    加入活动表SQL

--- common/conf/db/db.sql
+++ common/conf/db/db.sql
@@ -1058,6 +1058,75 @@ CREATE TABLE C_SUPPLIER
    PRIMARY KEY (ID)
 );
 
+/*==============================================================*/
+/* Table: F_ACTIVITY                                            */
+/*==============================================================*/
+DROP TABLE IF EXISTS F_ACTIVITY;
+create table F_ACTIVITY (
+   ID                   INT8                 not null,
+   STATUS               VARCHAR(6)           null,
+   TITLE                VARCHAR(700)         null,
+   BEGIN_TIME           DATE                 null,
+   END_TIME             DATE                 null,
+   IMAGE                VARCHAR(300)         null,
+   URL                  VARCHAR(300)         null,
+   APP_ID               VARCHAR(16)          null,
+   CLICK_TIME           INT8                 null,
+   TYPE                 VARCHAR(10)          null,
+   STYLE                VARCHAR(5)           null,
+   CREATE_TIME          TIMESTAMP            null,
+   UPDATE_TIME          TIMESTAMP            null,
+   CREATE_BY            VARCHAR(30)          null,
+   UPDATE_BY            VARCHAR(30)          null,
+   constraint PK_F_ACTIVITY primary key (ID)
+);
+
+comment on column F_ACTIVITY.ID is
+'主键';
+
+comment on column F_ACTIVITY.STATUS is
+'N表示正常   F表示禁用';
+
+comment on column F_ACTIVITY.TITLE is
+'活动标题';
+
+comment on column F_ACTIVITY.BEGIN_TIME is
+'活动开始时间';
+
+comment on column F_ACTIVITY.END_TIME is
+'活动结束时间';
+
+comment on column F_ACTIVITY.IMAGE is
+'活动图片';
+
+comment on column F_ACTIVITY.URL is
+'HTML5路径';
+
+comment on column F_ACTIVITY.APP_ID is
+'投放产品ID';
+
+comment on column F_ACTIVITY.CLICK_TIME is
+'点击次数';
+
+comment on column F_ACTIVITY.TYPE is
+'活动类型';
+
+comment on column F_ACTIVITY.STYLE is
+'活动形式';
+
+comment on column F_ACTIVITY.CREATE_TIME is
+'创建时间';
+
+comment on column F_ACTIVITY.UPDATE_TIME is
+'修改时间';
+
+comment on column F_ACTIVITY.CREATE_BY is
+'创建人';
+
+comment on column F_ACTIVITY.UPDATE_BY is
+'修改人';
+
+
 -- ----------------------------
 -- Alter Sequences Owned By 
 -- ----------------------------
