e63680619134a915008165dd0aace048a9229dbf
tangweiqun <weiqun.tang@sunlights.cc>
2014-11-20 15:49:18 +0800

    dq  Sql  活动数据库脚本

--- common/conf/db/db.sql
+++ common/conf/db/db.sql
@@ -1210,6 +1210,108 @@ COMMENT ON COLUMN F_GET_REWARD_RULE.update_by IS
 '修改人';
 
 
+DROP TABLE IF EXISTS F_REWARD_COUNT;
+/*==============================================================*/
+/* Table: F_REWARD_COUNT                                        */
+/*==============================================================*/
+create table F_REWARD_COUNT
+(
+   ID                   INT8                           not null,
+   customer_Id          VARCHAR(30)                    null,
+   REWARD_TYPE          VARCHAR(6)                     null,
+   GET_AMOUNT           INT8                           null,
+   HOLD_REWARD          INT8                           null,
+   FROZEN_REWARD        INT8                           null,
+   GET_MONEY            DECIMAL(18,2)                  null,
+   HOLD_MONEY           DECIMAL(18,2)                  null,
+   FROZEN_MONEY         DECIMAL(18,2)                  null,
+   CREATE_TIME          TIMESTAMP                      null,
+   UPDATE_TIME          TIMESTAMP                      null,
+   constraint PK_F_REWARD_COUNT primary key (ID)
+);
+
+comment on column F_REWARD_COUNT.ID is
+'主键';
+
+comment on column F_REWARD_COUNT.REWARD_TYPE is
+'奖励类型';
+
+comment on column F_REWARD_COUNT.GET_AMOUNT is
+'获取奖励数量：获取各种奖励类型的奖励的总和';
+
+comment on column F_REWARD_COUNT.HOLD_REWARD is
+'持有奖励数量：现持有各种奖励类型的奖励的总和';
+
+comment on column F_REWARD_COUNT.FROZEN_REWARD is
+'冻结奖励数量';
+
+comment on column F_REWARD_COUNT.GET_MONEY is
+'获取奖励折现';
+
+comment on column F_REWARD_COUNT.HOLD_MONEY is
+'持有奖励折现';
+
+comment on column F_REWARD_COUNT.FROZEN_MONEY is
+'冻结奖励折现';
+
+comment on column F_REWARD_COUNT.CREATE_TIME is
+'创建时间';
+
+comment on column F_REWARD_COUNT.UPDATE_TIME is
+'修改时间';
+
+
+/*==============================================================*/
+/* Table: F_REWARD_FLOW                                         */
+/*==============================================================*/
+DROP TABLE IF EXISTS F_REWARD_FLOW;
+create table F_REWARD_FLOW
+(
+   ID                   INT8                           not null,
+   customer_Id          VARCHAR(30)                    null,
+   ACTIVITY_ID          INT8                           null,
+   Activity_title       varchar(700)                   null,
+   SCENE                varchar(10)                    null,
+   STATUS               INT4                           null,
+   REWARD_TYPE          VARCHAR(6)                     null,
+   OPERATOR_TYPE        INT4                           null,
+   REWARD_AMT           INT8                           null,
+   MONEY                DECIMAL(18,2)                  null,
+   CREATE_TIME          TIMESTAMP                      null,
+   constraint PK_F_REWARD_FLOW primary key (ID)
+);
+
+comment on column F_REWARD_FLOW.ID is
+'主键';
+
+comment on column F_REWARD_FLOW.ACTIVITY_ID is
+'活动ID';
+
+comment on column F_REWARD_FLOW.Activity_title is
+'活动标题';
+
+comment on column F_REWARD_FLOW.SCENE is
+'应用场景';
+
+comment on column F_REWARD_FLOW.STATUS is
+'状态 0-未发放，1-已发放，2-发放失败，4-撤回成功，5-撤回失败 6-已兑换';
+
+comment on column F_REWARD_FLOW.REWARD_TYPE is
+'奖励类型';
+
+comment on column F_REWARD_FLOW.OPERATOR_TYPE is
+'1表示获取  2表示兑换';
+
+comment on column F_REWARD_FLOW.REWARD_AMT is
+'奖励数量';
+
+comment on column F_REWARD_FLOW.MONEY is
+'奖励折现';
+
+comment on column F_REWARD_FLOW.CREATE_TIME is
+'创建时间';
+
+
 -- ----------------------------
 -- Alter Sequences Owned By 
 -- ----------------------------
