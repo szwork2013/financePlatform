7ca9fa970793f2d7cd9e072b7e2ef8aec6399af3
wangjiaming <jiaming.wang@sunlights.cc>
2014-11-21 18:23:29 +0800

    SmsMessage add column successInd alter.sql

--- common/conf/db/modify.sql
+++ common/conf/db/modify.sql
@@ -1,4 +1,4 @@
 alter table c_customer alter column nick_name type character varying(50);
 alter table f_shumi_account alter column shumi_tokenKey type character varying(500);
 alter table f_shumi_account alter column shumi_tokenSecret type character varying(500);
-
+ALTER TABLE sms_message ADD COLUMN success_ind character varying(1);
