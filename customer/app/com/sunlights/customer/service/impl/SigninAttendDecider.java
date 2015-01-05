f5e29b2b1fdd0e913c613ef93b10dcf26dc4f376
tangweiqun <weiqun.tang@sunlights.cc>
2015-01-05 16:00:00 +0800

    活动bug修复

--- customer/app/com/sunlights/customer/service/impl/SigninAttendDecider.java
+++ customer/app/com/sunlights/customer/service/impl/SigninAttendDecider.java
@@ -20,6 +20,9 @@ public class SigninAttendDecider implements ActivityAttendDecider {
     public boolean decide(String custId, Map<String, List<CustJoinActivity>> listMap) {
         String currentDate = CommonUtil.dateToString(new Date(), CommonUtil.DATE_FORMAT_SHORT);
         List<CustJoinActivity> list = listMap.get(ActivityConstant.ACTIVITY_SIGNIN_SCENE_CODE);
+        if(list == null || list.isEmpty()) {
+            return true;
+        }
         for(CustJoinActivity custJoinActivity : list) {
             if(currentDate.equals(CommonUtil.dateToString(custJoinActivity.getCreateTime(), CommonUtil.DATE_FORMAT_SHORT))) {
                 return false;
