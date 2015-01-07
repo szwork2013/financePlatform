eff77cffaf839a6826e0a4b3a6eebc262534697f
wangjiaming <jiaming.wang@sunlights.cc>
2015-01-07 09:05:50 +0800

    login by gesture max error

--- customer/app/com/sunlights/customer/web/CustomerController.java
+++ customer/app/com/sunlights/customer/web/CustomerController.java
@@ -221,7 +221,7 @@ public class CustomerController extends Controller {
      *
      * @return
      */
-//    @With(MsgCenterAction.class)
+    @With(MsgCenterAction.class)
     public Result loginByges() {
         Logger.info("==========loginByGesture====================");
         Map<String, String> params = form().bindFromRequest().data();
@@ -249,7 +249,7 @@ public class CustomerController extends Controller {
         JsonNode json = MessageUtil.getInstance().toJson();
         Logger.info("==========loginByges返回：" + json.toString());
 
-//        response().setHeader(AppConst.HEADER_MSG, MessageUtil.getInstance().setMessageHeader(list));
+        response().setHeader(AppConst.HEADER_MSG, MessageUtil.getInstance().setMessageHeader(list));
 
         return Controller.ok(json);
     }
