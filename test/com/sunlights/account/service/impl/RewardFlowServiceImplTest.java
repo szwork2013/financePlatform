5feb8f951fcbdf852ee5448220c267eb8c026e7c
杨洪 <hong.yang@sunlights.cc>
2015-01-08 17:08:32 +0800

    account测试

--- test/com/sunlights/account/service/impl/RewardFlowServiceImplTest.java
+++ test/com/sunlights/account/service/impl/RewardFlowServiceImplTest.java
@@ -5,32 +5,26 @@ import com.sunlights.customer.service.RewardFlowService;
 import com.sunlights.customer.service.impl.RewardFlowServiceImpl;
 import models.RewardFlow;
 import org.junit.Test;
+import play.Logger;
 import play.db.jpa.JPA;
 import play.libs.F;
+import play.test.WithApplication;
 
 import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.fakeApplication;
 import static play.test.Helpers.running;
 
-public class RewardFlowServiceImplTest {
+public class RewardFlowServiceImplTest extends WithApplication {
 
     @Test
     public void testFindTodayFlowByCustIdAndScene() throws Exception {
-        running(fakeApplication(), new Runnable() {
-            public void run() {
-                JPA.withTransaction(new F.Callback0() {
-                    @Override
-                    public void invoke() throws Throwable {
+
                         RewardFlowService rewardFlowService = new RewardFlowServiceImpl();
                         RewardFlow rewardFlows = rewardFlowService.findTodayFlowByCustIdAndScene("20141119102210010000000029", "ASC002");
                         if(rewardFlows != null) {
-
+                            Logger.info("---------");
                             assertThat("20141119102210010000000029").isEqualTo(rewardFlows.getCustId());
                         }
-                    }
-                });
 
-            }
-        });
     }
 }
