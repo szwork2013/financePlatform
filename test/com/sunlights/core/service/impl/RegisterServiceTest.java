ced463908dd3abe9b1ebc153f9767ee138e3f258
wangjiaming <jiaming.wang@sunlights.cc>
2015-03-16 13:13:39 +0800

    test db

--- test/com/sunlights/core/service/impl/RegisterServiceTest.java
+++ test/com/sunlights/core/service/impl/RegisterServiceTest.java
@@ -1,77 +1,77 @@
-package com.sunlights.core.service.impl;
-
-import com.sunlights.DBUnitBasedTest;
-import org.dbunit.dataset.IDataSet;
-import org.junit.Test;
-
-import play.db.jpa.JPA;
-import play.libs.F;
-
-import com.sunlights.customer.service.LoginService;
-import com.sunlights.customer.service.impl.LoginServiceImpl;
-import com.sunlights.customer.vo.CustomerFormVo;
-
-/**
- * <p>Project: financeplatform</p>
- * <p>Title: RegisterServiceTest.java</p>
- * <p>Description: </p>
- * <p>Copyright (c) 2014 Sunlights.cc</p>
- * <p>All Rights Reserved.</p>
- *
- * @author <a href="mailto:jiaming.wang@sunlights.cc">wangJiaMing</a>
- */
-public class RegisterServiceTest extends DBUnitBasedTest {
-
-    @Test
-    public void registerTest() throws Throwable{
-
-        JPA.withTransaction("test", false, new F.Function0<Object>() {
-            @Override
-            public Object apply() throws Throwable {
-                CustomerFormVo customerFormVo = new CustomerFormVo();
-                customerFormVo.setMobilePhoneNo("15821948594");
-                customerFormVo.setPassWord("111111");
-                customerFormVo.setDeviceNo("deviceNo");
-
-                LoginService loginService = new LoginServiceImpl();
-                loginService.register(customerFormVo);
-
-                return null;
-            }
-        });
-
-        needRollbackData("c_customer", "c_authentication");
-        IDataSet dataSet = getExpectedDataSet("registerExpected.xml");
-
-        assertDataSet(dataSet, "c_authentication", "password,mobile");
-        assertDataSet(dataSet, "c_customer", "login_password,mobile");
-
-    }
-
-
-    @Test
-    public void registerP2PTest() throws Throwable{
-
-        JPA.withTransaction("test", false, new F.Function0<Object>() {
-            @Override
-            public Object apply() throws Throwable {
-                CustomerFormVo customerFormVo = new CustomerFormVo();
-                customerFormVo.setMobilePhoneNo("15821948594");
-                customerFormVo.setPassWord("111111");
-                customerFormVo.setChannel("1");
-
-                LoginService loginService = new LoginServiceImpl();
-                loginService.register(customerFormVo);
-
-                return null;
-            }
-        });
-
-        needRollbackData("c_customer", "c_authentication");
-        IDataSet dataSet = getExpectedDataSet("registerExpected2.xml");
-
-        assertDataSet(dataSet, "c_authentication", "password,mobile,channel");
-        assertDataSet(dataSet, "c_customer", "login_password,mobile");
-
-    }
-}
+//package com.sunlights.core.service.impl;
+//
+//import com.sunlights.DBUnitBasedTest;
+//import org.dbunit.dataset.IDataSet;
+//import org.junit.Test;
+//
+//import play.db.jpa.JPA;
+//import play.libs.F;
+//
+//import com.sunlights.customer.service.LoginService;
+//import com.sunlights.customer.service.impl.LoginServiceImpl;
+//import com.sunlights.customer.vo.CustomerFormVo;
+//
+///**
+// * <p>Project: financeplatform</p>
+// * <p>Title: RegisterServiceTest.java</p>
+// * <p>Description: </p>
+// * <p>Copyright (c) 2014 Sunlights.cc</p>
+// * <p>All Rights Reserved.</p>
+// *
+// * @author <a href="mailto:jiaming.wang@sunlights.cc">wangJiaMing</a>
+// */
+//public class RegisterServiceTest extends DBUnitBasedTest {
+//
+//    @Test
+//    public void registerTest() throws Throwable{
+//
+//        JPA.withTransaction("test", false, new F.Function0<Object>() {
+//            @Override
+//            public Object apply() throws Throwable {
+//                CustomerFormVo customerFormVo = new CustomerFormVo();
+//                customerFormVo.setMobilePhoneNo("15821948594");
+//                customerFormVo.setPassWord("111111");
+//                customerFormVo.setDeviceNo("deviceNo");
+//
+//                LoginService loginService = new LoginServiceImpl();
+//                loginService.register(customerFormVo);
+//
+//                return null;
+//            }
+//        });
+//
+//        needRollbackData("c_customer", "c_authentication");
+//        IDataSet dataSet = getExpectedDataSet("registerExpected.xml");
+//
+//        assertDataSet(dataSet, "c_authentication", "password,mobile");
+//        assertDataSet(dataSet, "c_customer", "login_password,mobile");
+//
+//    }
+//
+//
+//    @Test
+//    public void registerP2PTest() throws Throwable{
+//
+//        JPA.withTransaction("test", false, new F.Function0<Object>() {
+//            @Override
+//            public Object apply() throws Throwable {
+//                CustomerFormVo customerFormVo = new CustomerFormVo();
+//                customerFormVo.setMobilePhoneNo("15821948594");
+//                customerFormVo.setPassWord("111111");
+//                customerFormVo.setChannel("1");
+//
+//                LoginService loginService = new LoginServiceImpl();
+//                loginService.register(customerFormVo);
+//
+//                return null;
+//            }
+//        });
+//
+//        needRollbackData("c_customer", "c_authentication");
+//        IDataSet dataSet = getExpectedDataSet("registerExpected2.xml");
+//
+//        assertDataSet(dataSet, "c_authentication", "password,mobile,channel");
+//        assertDataSet(dataSet, "c_customer", "login_password,mobile");
+//
+//    }
+//}
