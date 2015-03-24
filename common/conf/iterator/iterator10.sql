97de42f4560dd61caea5a631660a877bfd33f70d
wangjiaming <jiaming.wang@sunlights.cc>
2015-03-24 09:05:51 +0800

    dbunit
    Summary:
    Login test
    
    Merge branch 'master' into dbunit_login
    
    Reviewers: loki
    
    Reviewed By: loki
    
    Subscribers: loki, wangjiaming
    
    Differential Revision: http://phab.sunlights.me/D69

--- common/conf/iterator/iterator10.sql
+++ common/conf/iterator/iterator10.sql
@@ -65,54 +65,38 @@ update c_customer set authentication_id = id;
 
 
 --------2015-03-18--------------
+DROP TABLE f_reward_account_balance;
+CREATE TABLE f_reward_account_balance ( id INTEGER NOT NULL, customer_id CHARACTER VARYING(30), reward_account_balance NUMERIC(18,2), reward_income_balance NUMERIC(18,2), reward_expend_balance NUMERIC(18,2), create_time TIMESTAMP(6) WITHOUT TIME ZONE, update_time TIMESTAMP(6) WITHOUT TIME ZONE, CONSTRAINT pk_f_reward_nce_id PRIMARY KEY (id) );
+COMMENT ON TABLE f_reward_account_balance IS '奖励账户余额';
+COMMENT ON COLUMN f_reward_account_balance.id IS '编号';
+COMMENT ON COLUMN f_reward_account_balance.customer_id IS '客户ID';
+COMMENT ON COLUMN f_reward_account_balance.reward_account_balance IS '奖励账户余额';
+COMMENT ON COLUMN f_reward_account_balance.reward_income_balance IS '奖励收入金额';
+COMMENT ON COLUMN f_reward_account_balance.reward_expend_balance IS '奖励支出兑换金额';
+COMMENT ON COLUMN f_reward_account_balance.create_time IS '创建时间';
+COMMENT ON COLUMN f_reward_account_balance.update_time IS '修改时间';
 
---奖励账户余额
-create table  f_reward_account_balance
-(
-       id                INTEGER not null,
-       customer_id       INTEGER,
-       reward_account_balance INTEGER,
-       reward_income_balance INTEGER,
-       reward_expend_balance INTEGER,
-       create_time       timestamp,
-       update_time       timestamp
-);
-alter  table f_reward_account_balance
-       add constraint PK_f_reward_nce_id primary key (id);
-comment on table f_reward_account_balance is '奖励账户余额';
-comment on column f_reward_account_balance.id is '编号';
-comment on column f_reward_account_balance.customer_id is '客户ID';
-comment on column f_reward_account_balance.reward_account_balance is '奖励账户余额';
-comment on column f_reward_account_balance.reward_income_balance is '奖励收入金额';
-comment on column f_reward_account_balance.reward_expend_balance is '奖励支出兑换金额';
-comment on column f_reward_account_balance.create_time is '创建时间';
-comment on column f_reward_account_balance.update_time is '修改时间';
+DROP TABLE f_reward_account_details;
+CREATE TABLE f_reward_account_details ( id INTEGER NOT NULL, customer_id CHARACTER VARYING(30), reward_sequence CHARACTER VARYING(20), activity_type CHARACTER VARYING(10), reward_type CHARACTER VARYING(10), reward_amount_balance BIGINT, income_expend_balance NUMERIC(18,2), fund_flow_type CHARACTER VARYING(10), create_time TIMESTAMP(6) WITHOUT TIME ZONE, update_time TIMESTAMP(6) WITHOUT TIME ZONE, CONSTRAINT pk_f_reward_ils_id PRIMARY KEY (id) );
+COMMENT ON TABLE f_reward_account_details IS '奖励账户余额明细';
+COMMENT ON COLUMN f_reward_account_details.id IS '编号';
+COMMENT ON COLUMN f_reward_account_details.customer_id IS '客户编号';
+COMMENT ON COLUMN f_reward_account_details.reward_sequence IS '奖励流水 以R开头年月日时分秒+递增数字或IDR2015031709531';
+COMMENT ON COLUMN f_reward_account_details.activity_type IS '活动类型 0-签到奖励，1-首次购买奖励，2-红包，3-推荐好友';
+COMMENT ON COLUMN f_reward_account_details.reward_type IS '奖励类型';
+COMMENT ON COLUMN f_reward_account_details.income_expend_balance IS '奖励支出收入金额';
+COMMENT ON COLUMN f_reward_account_details.fund_flow_type IS '资金流向 0-奖励收入，1-奖励支出(兑换)';
+COMMENT ON COLUMN f_reward_account_details.create_time IS '创建时间';
+COMMENT ON COLUMN f_reward_account_details.update_time IS '修改时间';
 
 
---奖励账户余额明细
-create table  f_reward_account_details
-(
-       Id                INTEGER not null,
-       customer_id       INTEGER,
-       reward_sequence   VARCHAR(20),
-       activity_type     VARCHAR(10),
-       reward_type       VARCHAR(10),
-       reward_amount_balance INTEGER,
-       income_expend_balance INTEGER,
-       fund_flow_type    VARCHAR(10),
-       create_time       timestamp,
-       update_time       timestamp
-);
-alter  table f_reward_account_details
-       add constraint PK_f_reward_ils_Id primary key (Id);
-comment on table f_reward_account_details is '奖励账户余额明细';
-comment on column f_reward_account_details.Id is '编号';
-comment on column f_reward_account_details.customer_id is '客户编号';
-comment on column f_reward_account_details.reward_sequence is '奖励流水 以R开头年月日时分秒+递增数字或IDR2015031709531';
-comment on column f_reward_account_details.activity_type is '活动类型 0-签到奖励，1-首次购买奖励，2-红包，3-推荐好友';
-comment on column f_reward_account_details.reward_type is '奖励类型';
-comment on column f_reward_account_details.reward_amount_balance is '奖励数量金额 红包奖励金额、签到奖励金豆数量';
-comment on column f_reward_account_details.income_expend_balance is '奖励支出收入金额';
-comment on column f_reward_account_details.fund_flow_type is '资金流向 0-奖励收入，1-奖励支出(兑换)';
-comment on column f_reward_account_details.create_time is '创建时间';
-comment on column f_reward_account_details.update_time is '修改时间';
+--奖励账户余额数据迁移
+insert into f_reward_account_balance select nextval('src'),a.customer_id,a.holdSum,a.getSum,a.frozenSum,now() from (
+select customer_id, sum(get_money - frozen_money) holdSum, sum(get_money) getSum,  sum(frozen_money) frozenSum from f_reward_count group by customer_id) a;
+
+--奖励账户明细数据迁移
+insert into f_reward_account_details
+select nextval('src'), a.customer_id, 'R' || to_char(current_timestamp, 'YYYYMMDDHH24MISS') || nextval('src'),a.scene,a.reward_type,a.reward_amt, a.money, a.fundType,a.create_time from
+(select customer_id,scene,reward_type,reward_amt, money, '0' fundType,create_time from f_reward_flow where operator_type = 1
+union
+select customer_id,scene,reward_type,reward_amt, money, '1' fundType,create_time from f_reward_flow where operator_type = 2 and status = 7) a;
