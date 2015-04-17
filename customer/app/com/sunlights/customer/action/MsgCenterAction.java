7044e8589434fe20dbfc5074a6e917f0f359cb71
wangjiaming <jiaming.wang@sunlights.cc>
2015-04-17 11:12:04 +0800

    ws p2p register url modify

--- customer/app/com/sunlights/customer/action/MsgCenterAction.java
+++ customer/app/com/sunlights/customer/action/MsgCenterAction.java
@@ -3,10 +3,7 @@ package com.sunlights.customer.action;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.Lists;
 import com.sunlights.common.AppConst;
-import com.sunlights.common.exceptions.BusinessRuntimeException;
 import com.sunlights.common.vo.MessageHeaderVo;
-import com.sunlights.customer.service.impl.CustomerService;
-import models.CustomerSession;
 import play.Logger;
 import play.db.jpa.JPA;
 import play.libs.F;
@@ -42,10 +39,10 @@ public class MsgCenterAction extends Action.Simple {
                 return result;
             }
 
-            final String token = getResponseToken(context);
-            if (token == null) {
-                return result;
-            }
+//            final String token = getResponseToken(context);
+//            if (token == null) {
+//                return result;
+//            }
             JsonNode jsonNode = Json.parse(headerMsg).get("headerValue");
             final List<MessageHeaderVo> messageHeaderVoList = Lists.newArrayList();
             if (jsonNode != null && jsonNode.isArray()) {
@@ -61,12 +58,12 @@ public class MsgCenterAction extends Action.Simple {
             JPA.withTransaction(new F.Callback0() {
                 @Override
                 public void invoke() throws Throwable {
-                    CustomerService customerService = new CustomerService();
-                    CustomerSession customerSession = customerService.getCustomerSession(token);
-                    if (customerSession == null) {
-                        Logger.info(">>无效的token,非法入侵！");
-                        throw new BusinessRuntimeException(">>无效的token,非法入侵！");
-                    }
+//                    CustomerService customerService = new CustomerService();
+//                    CustomerSession customerSession = customerService.getCustomerSession(token);
+//                    if (customerSession == null) {
+//                        Logger.info(">>无效的token,非法入侵！");
+//                        throw new BusinessRuntimeException(">>无效的token,非法入侵！");
+//                    }
                     MsgCenterActionService msgService = new MsgCenterActionService();
                     Logger.info(">>开始查询待发消息>>");
                     msgService.sendMsg(routeActionMethod, messageHeaderVoList);
