eaada55d6a70fdd0e0fb5b6073f3170a8904b99e
tangweiqun <weiqun.tang@sunlights.cc>
2015-05-15 15:04:21 +0800

    验证码bug修复

--- customer/app/com/sunlights/customer/service/impl/LoginServiceImpl.java
+++ customer/app/com/sunlights/customer/service/impl/LoginServiceImpl.java
@@ -148,6 +148,7 @@ public class LoginServiceImpl implements LoginService {
         String channel = vo.getChannel();
 
         boolean fromAppFlag = fromApp(channel);
+        boolean isVerifyCode = isVerifyCode(channel);
 
         Logger.info("=============recommendPhone:" + vo.getRecommendPhone());
         if (fromAppFlag) {
@@ -161,7 +162,7 @@ public class LoginServiceImpl implements LoginService {
             throw CommonUtil.getInstance().errorBusinessException(MsgCode.PHONE_NUMBER_ALREADY_REGISTRY);
         }
 
-        if (fromAppFlag && !isVerifyCodeRight(mobilePhoneNo, verifyCode, deviceNo)){
+        if (isVerifyCode && !isVerifyCodeRight(mobilePhoneNo, verifyCode, deviceNo)){
             return null;
         }
 
@@ -185,6 +186,13 @@ public class LoginServiceImpl implements LoginService {
 
         return customer;
 	}
+
+    private boolean isVerifyCode(String channel) {
+        if(AppConst.CHANNEL_PC.equals(channel)) {
+            return false;
+        }
+        return true ;
+    }
 
     private boolean isVerifyCodeRight(String mobilePhoneNo, String verifyCode, String deviceNo) {
         CustomerVerifyCodeVo customerVerifyCodeVo = new CustomerVerifyCodeVo();
